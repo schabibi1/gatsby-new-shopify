f55f057b76cc865828b84c9b8fc2518c6b75e953
Arisa Fukuzaki <25793187+schabibi1@users.noreply.github.com>
2021-09-21 17:19:21 +0200

    add: README and the before and after trick for a live coding in index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,19 +4,26 @@ import { Layout } from "../components/layout"
 import { sbEditable } from "@storyblok/storyblok-editable"
 import DynamicComponent from "../components/dynamicComponent"
 import useStoryblok from '../utils/storyblok'
+// import { ProductListing } from "../components/product-listing"
 
 export const query = graphql`
   query {
     storyblokEntry(full_slug: { eq: "home" }) {
       content
       full_slug
-    }
+    },
+    # shopifyCollection(handle: { eq: "frontpage" }) {
+    #   products {
+    #     ...ProductCard
+    #   }
+    # }
   }
 `
 
 export default function IndexPage({ data, location }) {
   let story = data.storyblokEntry
   story = useStoryblok(story, location)
+  // story.content = JSON.parse(story.content)
 
   const components = story.content.body.map(blok => {
     return (<DynamicComponent blok={blok} key={blok._uid} />)
@@ -26,6 +33,8 @@ export default function IndexPage({ data, location }) {
     <Layout>
       <div {...sbEditable(story.content)}>
         { components }
+      {/* <div>
+        <ProductListing products={data?.shopifyCollection?.products} /> */}
       </div>
     </Layout>
   )
